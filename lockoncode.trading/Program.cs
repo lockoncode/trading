49b70d072201c1c8257e5028b580bc6804c169c9
Neil Lock <neillock@yahoo.com>
2021-07-25 23:23:30 +1000

    Initial

---
+++ lockoncode.trading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lockoncode.trading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
